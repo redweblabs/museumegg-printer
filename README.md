d3e32fb1e624790c5fa9947b2e44164d7e30c290
Michael Craddock <michael@moblimic.com>
2017-02-22 18:26:54 +0000

    repo and code clean up

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# smhack-redweb
+# Museum Egg
+
+Api and printer processing application made at the Science Museum hack 2017 #SMHack 
+
+# Getting started
+
+- Add ```supersweetcreds.json``` to the  ```config``` folder
+- Run ```npm install```
+- Run ```npm install -g nodemon``` if not already installed
+- Run nodemon in root directory and access the test page on [http://localhost:3000](http://localhost:3000)
+
+# Associating repos
+
+- [Repo1]()
+- [Repo2]()
+
+#Team
+
+[Michael Craddock](https://github.com/moblimic)
+
+[Louie Colgan](https://github.com/ljbc1994)
+
+[Matt Barber](https://github.com/matt-barber)
+
+[Brandon Hawkes](https://github.com/bhawkes)
+
+[David Burton](https://github.com/Phishtitz)
+
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,78 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request');
-var path = require('path');
-var printer = require('printer');
-var fs = require('fs');
-var webshot = require('webshot');
-var admin = require("firebase-admin");
+var express = require('express'),
+    router = express.Router(),
+    request = require('request'),
+    path = require('path'),
+    printer = require('printer'),
+    fs = require('fs'),
+    webshot = require('webshot'),
+    admin = require("firebase-admin");
 
+// firebase
+admin.initializeApp({
+    credential: admin.credential.cert(path.join(__dirname, '../', 'config/') + "supersweetcreds.json"),
+    databaseURL: "https://smhack-159507.firebaseio.com"
+});
+
+var auth = admin.auth();
+var db = admin.database();
+var data = db.ref();
+
+// globals
 var fire = false;
 var dataCache = {};
 
-function printResults(file) {
+function widthCheck(val) {
+    // make sure everything's landscape
+    console.log('Checking widths');
+    var width = 0;
+    var height = 0;
+    val.measurements.dimensions.forEach(function (dimen) {
+        if (dimen.dimension.toLowerCase() == 'width') {
+            width = dimen.value;
+        } else {
+            height = dimen.value;
+        }
+    });
 
+    return width > height;
+}
+
+function buildImg(img) {
+    // some of the API data was coming through a bit funcky because of caching issues
+    // this solves it and builds an object with the url and dimensions for later use
+
+    console.log('Building the image');
+
+    var newImg = {};
+
+    var width = 0;
+    var height = 0;
+    img.measurements.dimensions.forEach(function (dimen) {
+        if (dimen.dimension.toLowerCase() == 'width') {
+            width = dimen.value;
+        } else {
+            height = dimen.value;
+        }
+    });
+
+    if (img.location.indexOf('http') !== -1) {
+        newImg.url = img.location;
+    } else {
+        newImg.url = 'http://smgco-images.s3.amazonaws.com/media/' + img.location;
+    }
+
+    newImg.width = width;
+    newImg.height = height;
+
+    return newImg;
+}
+
+function printResults(file) {
+    // print all the things!
     printer.printFile({
         filename: file,
         success: function (jobID) {
-            console.log("sent to printer with ID: " + jobID);
+            console.log("Sent to printer with ID: " + jobID);
         },
         error: function (err) {
             console.log(err);
@@ -24,7 +81,7 @@ function printResults(file) {
 }
 
 function processAPI(cb, args) {
-
+    // Our small selection of exhibits which are mapped to the beacons
     var items = [
         'co8421531',
         'co8359400',
@@ -36,89 +93,12 @@ function processAPI(cb, args) {
         'co34242'
     ];
 
-
     if (Object.keys(dataCache).length === 0) {
-        request({
-            url: 'http://collection.sciencemuseum.org.uk/objects/co8448547/motorola-microtac-classic-mobile-telephone-1991-2000-mobile-telephone',
-            headers: {
-                accept: 'application/vnd.api+json'
-            },
-            gzip: true
-        }, function (error, response, body) {
-            var data = JSON.parse(body);
-            var item = data.data;
-            console.log(item);
+        console.log('Making data cache');
 
-            // item primary description
-            item.attributes.description.map(function (i) {
-                if (i.primary === true) {
-                    item.attributes.description_primary = i.value;
-                }
-
-                return i;
-            });
-
-            function widthCheck(val) {
-                var width = 0;
-                var height = 0;
-                val.measurements.dimensions.forEach(function (dimen) {
-                    if (dimen.dimension.toLowerCase() == 'width') {
-                        width = dimen.value;
-                    } else {
-                        height = dimen.value;
-                    }
-                });
-
-                return width > height;
-            }
-
-            function buildImg(img) {
-                var newImg = {};
-
-                var width = 0;
-                var height = 0;
-                img.measurements.dimensions.forEach(function (dimen) {
-                    if (dimen.dimension.toLowerCase() == 'width') {
-                        width = dimen.value;
-                    } else {
-                        height = dimen.value;
-                    }
-                });
-
-                if (img.location.indexOf('http') !== -1) {
-                    newImg.url = img.location;
-                } else {
-                    newImg.url = 'http://smgco-images.s3.amazonaws.com/media/' + img.location;
-                }
-
-                newImg.width = width;
-                newImg.height = height;
-
-                return newImg;
-            }
-
-            // best image filtering
-            var images = item.attributes.multimedia;
-            var randImg,
-                imageSize = 'large',
-                landscape = false;
-
-            randImg = images[Math.floor(Math.random() * images.length)];
-
-            landscape = widthCheck(randImg.processed.large);
-
-            // only re check once
-            if (landscape === false) {
-                randImg = images[Math.floor(Math.random() * images.length)];
-            }
-
-            secretItem = {
-                name: item.attributes.summary_title,
-                description: item.attributes.description_primary,
-                image: buildImg(randImg.processed.large)
-            };
-        });
-
+        /*
+         main loop
+         */
         request({
             url: 'https://collection.sciencemuseum.org.uk/search?filter%5Bhas_image%5D=true&filter%5Bmuseum%5D=Science%20Museum&filter%5Bgallery%5D=Information%20Age%20Gallery%3A%20Web&page%5Bsize%5D=50&page%5Btype%5D=search',
             headers: {
@@ -139,46 +119,7 @@ function processAPI(cb, args) {
                             return i;
                         });
 
-                        function widthCheck(val) {
-                            var width = 0;
-                            var height = 0;
-                            val.measurements.dimensions.forEach(function (dimen) {
-                                if (dimen.dimension.toLowerCase() == 'width') {
-                                    width = dimen.value;
-                                } else {
-                                    height = dimen.value;
-                                }
-                            });
-
-                            return width > height;
-                        }
-
-                        function buildImg(img) {
-                            var newImg = {};
-
-                            var width = 0;
-                            var height = 0;
-                            img.measurements.dimensions.forEach(function (dimen) {
-                                if (dimen.dimension.toLowerCase() == 'width') {
-                                    width = dimen.value;
-                                } else {
-                                    height = dimen.value;
-                                }
-                            });
-
-                            if (img.location.indexOf('http') !== -1) {
-                                newImg.url = img.location;
-                            } else {
-                                newImg.url = 'http://smgco-images.s3.amazonaws.com/media/' + img.location;
-                            }
-
-                            newImg.width = width;
-                            newImg.height = height;
-
-                            return newImg;
-                        }
-
-                        // best image filtering
+                        // Find a random image and check its landscape
                         var images = item.attributes.multimedia;
                         var randImg,
                             imageSize = 'large',
@@ -188,7 +129,8 @@ function processAPI(cb, args) {
 
                         landscape = widthCheck(randImg.processed.large);
 
-                        // only re check once
+                        // This is just in case there's a portrait image. I will only recheck once because i couldn't
+                        // recheck and randomise the image in a loop because node was having a funny on me
                         if (landscape === false) {
                             randImg = images[Math.floor(Math.random() * images.length)];
                         }
@@ -213,11 +155,67 @@ function processAPI(cb, args) {
     }
 }
 
+function refreshCache() {
+    dataCache = {};
+    processAPI();
+}
+
 processAPI();
 
 function dockEgg(eggData, res) {
     console.log('Egg Docked');
-    // console.log(dataCache);
+
+    /*
+     random secret item
+     */
+    request({
+        url: 'http://collection.sciencemuseum.org.uk/search?filter%5Bmuseum%5D=Science%20Museum&filter%5Bgallery%5D=Information%20Age%20Gallery%3A%20Cell&page%5Bsize%5D=50&page%5Btype%5D=search',
+        headers: {
+            accept: 'application/vnd.api+json'
+        },
+        gzip: true
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+
+            var randItem = Math.floor(Math.random(0, Object.keys(data.data).length) * Object.keys(data.data).length);
+
+            var item = data.data[randItem];
+            // item primary description
+            item.attributes.description.map(function (i) {
+                if (i.primary === true) {
+                    item.attributes.description_primary = i.value;
+                }
+
+                return i;
+            });
+
+            // best image filtering
+            var images = item.attributes.multimedia;
+            var randImg,
+                imageSize = 'large',
+                landscape = false;
+
+            randImg = images[Math.floor(Math.random() * images.length)];
+
+            landscape = widthCheck(randImg.processed.large);
+
+            // only re check once
+            if (landscape === false) {
+                randImg = images[Math.floor(Math.random() * images.length)];
+            }
+
+            secretItem = {
+                name: item.attributes.summary_title,
+                description: item.attributes.description_primary,
+                image: buildImg(randImg.processed.large)
+            };
+
+            console.log('Secret item found');
+
+        }
+    });
+
 
     // print selected resource
 
@@ -234,8 +232,6 @@ function dockEgg(eggData, res) {
         console.log(ind);
         dwell_artifacts[ind].dwell = Math.round(val[Object.keys(val)[0]].score);
     });
-
-    console.log(dwell_artifacts);
 
     var total = dwell_artifacts.reduce(function (t, y) {
         return t + y.dwell
@@ -289,7 +285,6 @@ function dockEgg(eggData, res) {
             }
         }, function (err) {
             printResults(printName);
-            // res.status(200).send(html);
         });
 
     });
@@ -297,21 +292,12 @@ function dockEgg(eggData, res) {
 
 }
 
-
-admin.initializeApp({
-    credential: admin.credential.cert(path.join(__dirname, '../', 'config/') + "supersweetcreds.json"),
-    databaseURL: "https://smhack-159507.firebaseio.com"
-});
-
-var auth = admin.auth();
-var db = admin.database();
-var data = db.ref();
-
-
-/// printer
+/*
+ Firebase code for receiving egg data
+ */
 data.limitToLast(1).on("child_added", function (snapshot) {
     if (fire === true) {
-        console.log('LOLOLOL');
+        console.log('Data recieved from Firebase');
         var eggData = snapshot.val();
 
         request.post('http://localhost:3000/', {
@@ -324,7 +310,9 @@ data.limitToLast(1).on("child_added", function (snapshot) {
     }
 });
 
+// test router for forcing print wiouth egg
 router.get('/test', function (req, res, next) {
+    console.log('test fired');
     request.post('http://localhost:3000/', {
         json: {
             eggdata: [
@@ -357,11 +345,11 @@ router.get('/test', function (req, res, next) {
         }
     });
 
-    res.status(200).send('<strong>k</strong>');
+    res.status(200).send('<h1>Test data printed!</h1>');
 });
 
+// Main router to build view for printing
 router.post('/', function (req, res, next) {
-    console.log('post /');
     dockEgg(req.body.eggdata, res);
 });
 
